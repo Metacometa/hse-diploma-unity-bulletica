0d711fd491e2338fc21baa0102bcf330eb2876ad
Kirill <kirillstoker@gmail.com>
2025-05-04 17:02:07 +0300

    Add medium chamber, edit start chamber

---
+++ Assets/Assets/Scripts/Monobehaviour/Light/PingPongLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PingPongLight : MonoBehaviour
+{
+    private Light2D lightComponent;
+
+    [SerializeField] private float minIntensity;
+    [SerializeField] private float maxIntensinty;
+
+    void Awake()
+    {
+        lightComponent = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        lightComponent.intensity = Mathf.PingPong(Time.deltaTime, maxIntensinty) + minIntensity;
+    }
+}
